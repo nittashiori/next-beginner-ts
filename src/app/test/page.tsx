37baa828a0c865993cc685d75dbdee355f31da16
nittashiori <nitta@impv.co.jp>
2023-03-31 09:20:42 +0900

    Server Componentの動作確認

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,13 @@
+import UsersList from './UsersList';
+
 const Test = () => {
-  return <h1>Testページ</h1>;
+  return (
+    <>
+      <h1>Testページ</h1>
+      {/* @ts-expect-error Server Component */}
+      <UsersList />
+    </>
+  );
 };
 
 export default Test;
