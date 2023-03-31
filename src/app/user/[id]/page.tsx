c8396d4a35ef97c6ca534d92ed0fc3293fbb06c4
nittashiori <nitta@impv.co.jp>
2023-03-31 11:07:15 +0900

    Revalidating Data(ISR)の設定

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -13,7 +13,12 @@ type User = {
 const getUser = async(id: string) => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${id}`
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      }
+    }
   );
   const user: User = await response.json();
   return user;
@@ -41,6 +46,7 @@ const User = async({ params: { id } }: Props) => {
       <div>
         <div>名前： {user.name}</div>
         <div>メールアドレス： {user.email}</div>
+        <div>{Math.random()}</div>
       </div>
     </div>
   )
