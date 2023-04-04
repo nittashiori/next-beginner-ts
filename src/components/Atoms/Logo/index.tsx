1239d86186dcc7c114c2f0a78d11a2427e102fa5
nittashiori <nitta@impv.co.jp>
2023-04-04 13:55:13 +0900

    public配下の画像を表示させる

--- src/components/Atoms/Logo/index.tsx
+++ src/components/Atoms/Logo/index.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image"
+
 interface LogoProps {
   label: string;
 }
 
 export const Logo = ({label}: LogoProps) => {
   return (
-    <p>{label}</p>
+    <p>
+      {label}
+      <Image src="/vercel.svg" alt="vercel logo" width={384} height={75} />
+    </p>
   );
 };
