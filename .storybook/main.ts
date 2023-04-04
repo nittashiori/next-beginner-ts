a8bbcb471db31b3641f3496c7235b4ec3783ebd7
nittashiori <nitta@impv.co.jp>
2023-04-04 13:25:21 +0900

    storybook導入

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import type { StorybookConfig } from "@storybook/nextjs";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {
+      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
