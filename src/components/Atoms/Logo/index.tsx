907968cf80e0e011780f7e194133151c164d6ce9
nittashiori <nitta@impv.co.jp>
2023-04-13 10:39:49 +0900

    Revert "public配下の画像を表示させる"
    This reverts commit 1239d86186dcc7c114c2f0a78d11a2427e102fa5.

--- src/components/Atoms/Logo/index.tsx
+++ src/components/Atoms/Logo/index.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image"
-
 interface LogoProps {
   label: string;
 }
 
 export const Logo = ({label}: LogoProps) => {
   return (
-    <p>
-      {label}
-      <Image src="/vercel.svg" alt="vercel logo" width={384} height={75} />
-    </p>
+    <p>{label}</p>
   );
 };
