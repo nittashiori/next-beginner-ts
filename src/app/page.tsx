d0cb7f9ea78a15b09e0632b67725ea0ed924d65a
nittashiori <nitta@impv.co.jp>
2023-06-22 08:57:59 +0900

    表示件数を3件に調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getList } from "@/libs/microcms";
 import { formatDate } from '@/libs/dateUtils';
 
 export default async function Page() {
-  const { contents } = await getList();
+  const { contents } = await getList({ limit: 3 });
 
   const formattedPosts = contents.map(post => ({
     ...post,
