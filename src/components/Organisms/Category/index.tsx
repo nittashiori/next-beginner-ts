1d011d1662cbf4431fedfa65fddd4d1ec85a6a18
nittashiori <nitta@impv.co.jp>
2023-06-27 11:32:21 +0900

    スマホ版はselectボックスでカテゴリを絞り込む

--- src/components/Organisms/Category/index.tsx
+++ src/components/Organisms/Category/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import styles from './index.module.css';
 import Link from 'next/link';
 
@@ -26,27 +26,60 @@ export const Category: FC<Props> = ({
   categoryLists
 }) => {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    if(event.target.value === 'all') {
+      router.push('/news/1')
+    } else {
+      router.push(`/news/category/${event.target.value}/1`)
+    }
+  }
 
   return (
-    <ul className={styles.lists}>
-      <li>
-        <Link
-          href="/news/1"
-          className={pathname.includes("/news") && !pathname.includes("/category") ? styles.current : ''}
-        >
-          All
-        </Link>
-      </li>
-      {categoryLists.map((list, index) => (
-        <li key={index}>
-          <Link
-            href={`/news/category/${list.id}/1`}
-            className={pathname.includes(`${list.id}`) ? styles.current : ''}
-          >
-            {list.id}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className={styles.pc}>
+        <ul className={styles.lists}>
+          <li>
+            <Link
+              href="/news/1"
+              className={pathname.includes("/news") && !pathname.includes("/category") ? styles.current : ''}
+            >
+              All
+            </Link>
+          </li>
+          {categoryLists.map((list, index) => (
+            <li key={index}>
+              <Link
+                href={`/news/category/${list.id}/1`}
+                className={pathname.includes(`${list.id}`) ? styles.current : ''}
+              >
+                {list.id}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.sp}>
+        <p className={styles.label}>Category:</p>
+        <div className={styles.selectWrap}>
+          <select className={styles.select} onChange={handleOptionChange}>
+            <option value="all">All</option>
+            {categoryLists.map((list, index) => (
+              <option
+                key={index}
+                value={list.id}
+                selected={pathname.includes(`${list.id}`) && true}
+              >
+                {list.id}
+              </option>
+            ))}
+          </select>
+          <span className="material-icons">arrow_drop_down</span>
+        </div>
+      </div>
+    </>
   )
 }
